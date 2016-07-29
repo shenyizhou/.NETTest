64a5c38c9ac464f38316dd5b96455b9d81e05eae
shenyizhou <shenyizhou@126.com>
2016-07-29 14:55:46 +0800

    Add Process

---
+++ 2016年7月29日/Test001/Test001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Test001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] pro = Process.GetProcesses();
+            foreach(var item in pro)
+            {
+                Console.WriteLine(item.ProcessName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
