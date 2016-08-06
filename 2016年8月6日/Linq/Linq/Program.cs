6e612fc7fb2d87eef23e4e117b2a2962e0887f7a
shenyizhou <shenyizhou@126.com>
2016-08-06 21:55:07 +0800

    Add Linq

---
+++ 2016年8月6日/Linq/Linq/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] words = { "hello", "wonderful", "linq", "beautiful", "world" };
+            //var shortWords = from word in words where word.Length <= 5 select word;
+            //foreach (var word in shortWords)
+            //{
+            //    Console.WriteLine(word);
+            //}
+
+            string[] words = { "hello", "wonderful", "linq", "beautiful", "world" };
+
+            var groups = 
+                from word in words 
+                orderby word ascending 
+                group word by word.Length into lengthGroups 
+                orderby lengthGroups.Key descending 
+                select new { Length = lengthGroups.Key, words = lengthGroups };
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine("Words of length " + group.Length);
+                foreach (string word in group.words)
+                {
+                    Console.WriteLine(" " + word);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
